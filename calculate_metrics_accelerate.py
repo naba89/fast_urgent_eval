d6553f9591efe8de27e5a67c0665d0c5fbbc7175
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2025-08-25 18:14:33 +0900

    Update calculate_metrics_accelerate.py

--- calculate_metrics_accelerate.py
+++ calculate_metrics_accelerate.py
@@ -204,9 +204,9 @@ def compute_metrics(args, metrics, ref, inf, ref_sr, inf_sr, ref_txt, lang_id, u
 
     inf_48k = soxr.resample(inf_np, inf_sr, 48000)
 
-    ref_16k = torch.from_numpy(ref_16k).to(device)
-    inf_16k = torch.from_numpy(inf_16k).to(device)
-    inf_48k = torch.from_numpy(inf_48k).to(device)
+    ref_16k = torch.from_numpy(ref_16k).to(device).unsqueeze(0)
+    inf_16k = torch.from_numpy(inf_16k).to(device).unsqueeze(0)
+    inf_48k = torch.from_numpy(inf_48k).to(device).unsqueeze(0)
 
     ref = ref.to(device)
     inf = inf.to(device)
